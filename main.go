b88f2c9850d585aaefe9570fb35605ab4670579f
Faithfulness Alamu <vaguemail369@gmail.com>
2020-08-21 18:47:24 +0100

    Implement the http server

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
 
+func main() {
+	http.HandleFunc("/verify", verifyHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := net.JoinHostPort("", port)
+	log.Printf("Serving on %s...\n", addr)
+	http.ListenAndServe(addr, nil)
 }
